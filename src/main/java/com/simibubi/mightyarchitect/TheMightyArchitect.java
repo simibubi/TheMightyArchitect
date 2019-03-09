3ae57b38f839a8ba046195ffc4861a38ef124cbf
simibubi <simi@bubi.com>
2019-03-09 21:24:22 +0100

    update version

--- src/main/java/com/simibubi/mightyarchitect/TheMightyArchitect.java
+++ src/main/java/com/simibubi/mightyarchitect/TheMightyArchitect.java
@@ -34,7 +34,7 @@ public class TheMightyArchitect {
 
     public static final String ID = "mightyarchitect";
     public static final String NAME = "The Mighty Architect";
-    public static final String VERSION = "0.0.2";
+    public static final String VERSION = "0.1.0";
 
     @Mod.Instance
     public static TheMightyArchitect instance;
